1f4c8850488ebc209a25bea18c0ea07fc67b7a33
fikrisue07 <mhs.moh.fikrisue@umgo.ac.id>
2024-06-27 16:21:31 +0800

    Update test_restaurants.js

--- tests/test_restaurants.js
+++ tests/test_restaurants.js
@@ -1,1 +1,35 @@
+const puppeteer = require('puppeteer');
+
+describe('Frontend Tests', () => {
+    let browser;
+    let page;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    test('loads the home page', async () => {
+        await page.goto('http://localhost:3000');
+        await page.waitForSelector('h1');
+        const title = await page.$eval('h1', e => e.textContent);
+        expect(title).toBe('Restaurant Booking');
+    });
+
+    test('can make a reservation', async () => {
+        await page.goto('http://localhost:3000');
+        await page.type('#username', 'testuser');
+        await page.type('#restaurant', 'Restaurant A');
+        await page.type('#date', '2024-07-01T19:00');
+        await page.type('#guests', '2');
+        await page.click('#submit');
+        await page.waitForSelector('.reservation');
+        const reservation = await page.$eval('.reservation', e => e.textContent);
+        expect(reservation).toContain('testuser');
+    });
+});
 
