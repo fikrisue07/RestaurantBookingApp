46e9ff86917a1b31eb992ad33d10a055cf836ad9
fikrisue07 <mhs.moh.fikrisue@umgo.ac.id>
2024-06-27 15:04:25 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Restaurant Booking API');
+});
+
+// Import routes
+const reservations = require('./routes/reservations');
+const users = require('./routes/users');
+const restaurants = require('./routes/restaurants');
+
+app.use('/api/reservations', reservations);
+app.use('/api/users', users);
+app.use('/api/restaurants', restaurants);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
